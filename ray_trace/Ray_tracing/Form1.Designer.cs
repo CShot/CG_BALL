6d19cd2e37533355db4b6f1c95d68ea345f62ce2
Максим Панков < >
2017-05-30 22:28:03 +0300

    CG

---
+++ ray_trace/Ray_tracing/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Ray_tracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(781, 479);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(884, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Радиус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(829, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(899, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "y";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(856, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(872, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(940, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(799, 74);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(974, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 513);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ray_trace/Ray_tracing/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Ray_tracing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            glControl1.Invalidate();
+        }
+
+        float v1 = 0;
+        float v2 = 0;
+        float v3 = -4;
+
+        float v1_v = 0;
+        float v2_v = 0;
+        float v3_v = -1;
+
+        int x;
+        int y;
+        int z;
+        int spheresVal = 3;
+        int radius;
+
+        void GlPaint()
+        {
+            Shaders m = new Shaders();
+
+            Console.WriteLine(m.glslVersion);
+            Console.WriteLine(m.glVersion);
+
+            m.InitShaders(glControl1.Width / (float)glControl1.Height);
+
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+
+
+            GL.EnableVertexAttribArray(m.attribute_vpos);
+
+
+            Console.WriteLine("OK");
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+
+
+            GL.DisableVertexAttribArray(m.attribute_vpos);
+
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+        }
+
+        void Relorad()
+        {
+            string str = string.Empty;
+            using (System.IO.StreamReader reader = System.IO.File.OpenText("..\\..\\ray_trace_example.frag"))
+            {
+                str = reader.ReadToEnd();
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("..\\..\\ray_trace.frag"))
+            {
+                file.Write(str);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Relorad();
+        }
+       
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            GlPaint();
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {  
+            Regex v3_r_s = new Regex("v3="+Convert.ToString(v3));//вперед    
+            Regex v3_r_w = new Regex("v3=" + Convert.ToString(v3));//назад
+
+            Regex v1_r_d = new Regex("v1=" + Convert.ToString(v1));//вправо   
+            Regex v1_r_a = new Regex("v1=" + Convert.ToString(v1));//влево
+
+            Regex v2_r_Q = new Regex("v2=" + Convert.ToString(v2));//вверх
+            Regex v2_r_E = new Regex("v2=" + Convert.ToString(v2));//вниз
+
+            Regex v2_r_i = new Regex("v2_v=" + Convert.ToString(v2_v));//смотреть вверх   
+            Regex v2_r_k = new Regex("v2_v=" + Convert.ToString(v2_v));//смотреть вниз
+
+            Regex v1_r_j = new Regex("v1_v=" + Convert.ToString(v1_v));//смотреть налево 
+            Regex v1_r_l = new Regex("v1_v=" + Convert.ToString(v1_v));//смотреть направо
+
+            Regex v3_r_p = new Regex("v3_v=" + Convert.ToString(v3_v));
+
+            string str = string.Empty;
+            using (System.IO.StreamReader reader = System.IO.File.OpenText("..\\..\\ray_trace.frag"))
+            {
+                str = reader.ReadToEnd();
+            }
+            if (e.KeyData == Keys.S)
+            {
+                v3 = v3 - 1;
+                str = v3_r_s.Replace(str, "v3=" + Convert.ToString(v3), 3); 
+            }
+            if (e.KeyData == Keys.W)
+            {
+                v3 = v3 + 1;
+                str = v3_r_w.Replace(str, "v3=" + Convert.ToString(v3), 3); 
+            }
+            if (e.KeyData == Keys.D)
+            {
+                v1 = v1 + 1;
+                str = v1_r_d.Replace(str, "v1=" + Convert.ToString(v1), 3); 
+            }
+            if (e.KeyData == Keys.A)
+            {
+                v1 = v1 - 1;
+                str = v1_r_d.Replace(str, "v1=" + Convert.ToString(v1), 3); 
+            }
+            if (e.KeyData == Keys.Q)
+            {
+                v2 = v2 + 1;
+                str = v2_r_Q.Replace(str, "v2=" + Convert.ToString(v2), 3); 
+            }
+            if (e.KeyData == Keys.E)
+            {
+                v2 = v2 - 1;
+                str = v2_r_E.Replace(str, "v2=" + Convert.ToString(v2), 3);   
+            }
+            if (e.KeyData == Keys.I)
+            {
+                v2_v = v2_v + 1;
+                str = v2_r_i.Replace(str, "v2_v=" + Convert.ToString(v2_v), 3);
+            }
+            if (e.KeyData == Keys.K)
+            {
+                v2_v = v2_v - 1;
+                str = v2_r_k.Replace(str, "v2_v=" + Convert.ToString(v2_v), 3);
+            }
+            if (e.KeyData == Keys.J)
+            {
+                v1_v = v1_v - 1;
+                str = v1_r_j.Replace(str, "v1_v=" + Convert.ToString(v1_v), 3);
+            }
+            if (e.KeyData == Keys.L)
+            {
+                v1_v = v1_v + 1;
+                str = v1_r_l.Replace(str, "v1_v=" + Convert.ToString(v1_v), 3);
+            }
+            if (e.KeyData == Keys.P)
+            {
+                v3_v = v3_v * -1;
+                str = v3_r_p.Replace(str, "v3_v=" + Convert.ToString(v3_v), 3);
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("..\\..\\ray_trace.frag"))
+            {
+                file.Write(str);
+            }
+            GlPaint();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radius = Convert.ToInt32(textBox1.Text);
+            x = Convert.ToInt32(textBox2.Text);
+            y = Convert.ToInt32(textBox3.Text);
+            z = Convert.ToInt32(textBox4.Text);
+
+            ////////////////
+
+            Regex spherVal_r = new Regex("spheresVal=" + Convert.ToString(spheresVal));//изменение количества шаров
+            spheresVal++;
+            string str = string.Empty;
+            using (System.IO.StreamReader reader = System.IO.File.OpenText("..\\..\\ray_trace.frag"))
+            {
+                str = reader.ReadToEnd();
+            }
+            str = spherVal_r.Replace(str, "spheresVal=" + Convert.ToString(spheresVal), 3);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("..\\..\\ray_trace.frag"))
+            {
+                file.Write(str);
+            }
+            ///////////////
+
+              int spehersVal_tmp = spheresVal - 2;
+              Regex sperCoord_r = new Regex("int kost;");//Ошибка где-то тут
+              string str_coord = string.Empty;
+              using (System.IO.StreamReader reader_coord = System.IO.File.OpenText("..\\..\\ray_trace.frag"))
+              {
+                  str_coord = reader_coord.ReadToEnd();
+              }
+              str_coord = sperCoord_r.Replace(str_coord, "spheres[" + Convert.ToString(spheresVal-1) + "].Center = vec3(" + Convert.ToString(x) + "," + Convert.ToString(y) + ", " + Convert.ToString(z) + ");spheres[" + Convert.ToString(spheresVal-1) + "].Radius = " + Convert.ToString(radius) + ";spheres[" + Convert.ToString(spheresVal-1) + "].Materialidx = 0; int kost;", 3);
+
+              using (System.IO.StreamWriter file_spehers = new System.IO.StreamWriter("..\\..\\ray_trace.frag"))
+              {
+                  file_spehers.Write(str_coord);
+              }
+              GlPaint();
+         
+             
+        }
+
+    }
+}
